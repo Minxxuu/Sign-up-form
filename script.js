810d95f56cb20ebe3de5f29b68422029f2da6309
ollie <henwoodo97@gmail.com>
2026-05-05 18:28:50 +0100

    Basic form and button created

---
+++ script.js
@@ -0,0 +1,11 @@
+const submit = document.getElementById('submit');
+const password = document.getElementById('password');
+const confirm_password = document.getElementById('confirm_password');
+
+submit.addEventListener('click', function(event) {
+    if (password.value !== confirm_password.value) {
+        event.preventDefault();
+        alert('Passwords do not match!');
+    }
+});
+
